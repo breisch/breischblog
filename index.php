1843d659dc2efb7f8f722cb248143a47fddebd8c
gvbh01 <gvbh01@DS0877685.sandviken.local>
2013-04-25 10:20:03 +0200

    blogg 2013-04-25

--- index.php
+++ index.php
@@ -63,18 +63,5 @@ $posts = (isset($_GET['id']))?get_posts($_GET['id']) : get_posts();
         
         ?>
     </div><!-- End wrapper -->
-    
-=======
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-
     </body>
 </html>
